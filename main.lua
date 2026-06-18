086d833fef78b1525614d33d1d85da16ef2d52b2
Siddhant Gurjar <akshitgurjar9026@gmail.com>
2026-06-18 22:10:25 +0530

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,20 @@
+local PlaceId = game.PlaceId
+
+local BloxFruitsIDs = {
+    [2753915549] = true,
+    [85211729168715] = true,
+    [4442272183] = true,
+    [79091703265657] = true,
+    [7449423635] = true,
+    [100117331123089] = true
+}
+
+if BloxFruitsIDs[PlaceId] then
+    if readfile and isfile and isfile("testing/BloxFruits.lua") then
+        loadstring(readfile("testing/BloxFruits.lua"))()
+    else
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/SiddhantGurjar/testsub/main/testing/BloxFruits.lua"))()
+    end
+else
+    warn("Unsupported Game PlaceId: " .. tostring(PlaceId))
+end
